fbdbc75178bab1ff40d2768dcebe3dc8b040e95a
Jean Sousa <jean.sousa@vexpenses.com>
2026-02-02 23:09:20 -0300

    Aula 04 - exercicios

--- exercicios/aula_04/main.py
+++ exercicios/aula_04/main.py
@@ -1,7 +1,11 @@
+# 8 - Crie um arquivo chamado main.py, importe a classe Livro e, no arquivo main.py, 
+# instancie dois objetos da classe Livro e exiba a mensagem formatada utilizando o método str.
+
 from exercicio_01 import Livro
 
 livro_main1 = Livro("Python para Iniciantes", "Carlos Coder", 2021)
 livro_main2 = Livro("Web Development Essentials", "Laura Developer", 2023)
 
+# print utilizando o metodo DUNDER METHOD (metodos com dois underescore) __str__
 print(livro_main1)
 print(livro_main2)
