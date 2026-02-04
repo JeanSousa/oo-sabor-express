2bd4ead6f1f31b338b665f79dc594cfeeb731967
Jean Sousa <jean.sousa@vexpenses.com>
2026-02-04 17:09:56 -0300

    Curso Avance orientacao a objetos Aula 04 filtrando dados

--- app.py
+++ app.py
@@ -15,7 +15,27 @@ if response.status_code == 200:
     # salvando os dados json em uma variavel
     # o metodo .json traz um json da resposta
     dados_json = response.json()
-    print(dados_json)
+    
+    # dicionario
+    dados_restaurante = {}
+
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+
+        # verificando se o nome do restaurante não existe no dicionario
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+
+        # adicionando itens ao dicionario para cada restaurante
+        dados_restaurante[nome_do_restaurante].append({
+            "item": item['Item'],
+            "price": item['price'],
+            "description": item['description']
+        })
+
+
+    # printando apenas um restaurante
+    print(dados_restaurante['McDonald’s'])
 
 else:
     print(f"O erro foi {response.status_code}")
